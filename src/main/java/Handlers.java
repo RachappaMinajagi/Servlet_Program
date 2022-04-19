8642b80a27538528e9071028713a8c61f00117d7
RachappaMinajagi <59776797+RachappaMinajagi@users.noreply.github.com>
2022-04-20 02:21:02 +0530

    Create Simple Web Server

---
+++ src/main/java/Handlers.java
@@ -0,0 +1,25 @@
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+
+import java.io.OutputStream;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+
+public class Handlers {
+    public static class RootHandler implements HttpHandler {
+
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            String response = "<h1>Server start success if you see this message</h1>" + "<h1>port:" +
+                    SimpleHttpServer.port + "</h1>";
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+
+    }
+
+}
---
+++ src/main/java/SimpleHttpServer.java
@@ -0,0 +1,32 @@
+import com.sun.net.httpserver.HttpsServer;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class SimpleHttpServer {
+    public static int DEFAULT_PORT = 8082;
+    public static int port;
+    private HttpsServer httpsServer;
+
+    private void start(int port) {
+        this.port = port;
+        try {
+            httpsServer = HttpsServer.create(new InetSocketAddress(port), 0);
+            System.out. println("server started at " + port);
+            httpsServer.createContext("/", new Handlers.RootHandler());
+//            httpsServer.createContext("/echoHeader", new Handlers.EchoHeaderHandler());
+//            httpsServer.createContext("/echoGet", new Handlers.EchoGetHandler());
+//            httpsServer.createContext("/echoPost", new Handlers.EchoPostHandler());
+            httpsServer.setExecutor(null);
+            httpsServer.start();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+
+        }
+    }
+    public static void main(String[] args) {
+        SimpleHttpServer httpServer = new SimpleHttpServer();
+        httpServer.start(DEFAULT_PORT);
+    }
+
+}
